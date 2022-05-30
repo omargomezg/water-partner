0bcac38ca65f4aeaa4ac00bcb690cc5fa779a66f
Omar Gomez Gomez <omar.gomez@outlook.cl>
2022-05-30 11:54:02 -0400

    set empty meters in conversion

--- src/main/java/com/hardnets/coop/convert/WaterMeterEntityToDto.java
+++ src/main/java/com/hardnets/coop/convert/WaterMeterEntityToDto.java
@@ -2,14 +2,17 @@ package com.hardnets.coop.convert;
 
 import com.hardnets.coop.model.dto.WaterMeterDto;
 import com.hardnets.coop.model.entity.WaterMeterEntity;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Component
 public class WaterMeterEntityToDto implements Converter<WaterMeterEntity, WaterMeterDto> {
     @Override
     public WaterMeterDto convert(WaterMeterEntity meter) {
+       log.info(meter);
         return WaterMeterDto.builder()
                 .id(meter.getId())
                 .serial(meter.getSerial())
